ebb3d51586bfbe7c8ec1caaf801b67b1e9325145
magno309 <48113838+magno309@users.noreply.github.com>
2020-11-09 16:08:48 -0600

    Corregido form de inicio

--- AnclaSystem/Vista/Program.cs
+++ AnclaSystem/Vista/Program.cs
@@ -17,7 +17,7 @@ namespace Vista
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmMenu());
+            Application.Run(new frmInicioSesion());
         }
     }
 }
