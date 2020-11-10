e9b7a89ae8445b872b3b41abb871d29dacf1cf40
Jesus Ramirez Ayala <48115771+jesusraayala1298@users.noreply.github.com>
2020-11-10 14:54:50 -0600

    Avance de ventas

--- AnclaSystem/Modelo/DAOVentas.cs
+++ AnclaSystem/Modelo/DAOVentas.cs
@@ -10,7 +10,7 @@ namespace Datos
 {
     public class daoVentas
     {
-        public bool agregarVenta(Ventas nuevaVenta, List<DetalleVentas> detalles)
+        public bool agregarVenta(Ventas nuevaVenta, List<DetalleVentas> detalleVenta)
         {
             MySqlConnection conexion = new MySqlConnection();
             MySqlTransaction trans = conexion.BeginTransaction();
@@ -29,46 +29,25 @@ namespace Datos
                 comando.Parameters.AddWithValue("TOTAL", nuevaVenta.TOTAL);
                 comando.Parameters.AddWithValue("FECHA", nuevaVenta.FECHA);
                 comando.ExecuteNonQuery();
-                comando.Dispose();
-                trans.Commit();
-                conexion.Close();
-                agregarDetalles(detalles);
-                return true;
-            }
-            catch (Exception e)
-            {
-                trans.Rollback();
-                conexion.Close();
-                return false;
-            }
-        }
-        public bool agregarDetalles(List<DetalleVentas> detalleVenta)
-        {
-            MySqlConnection conexion = new MySqlConnection();
-            MySqlTransaction trans = conexion.BeginTransaction();
-            try
-            {
 
-                String server = "25.89.125.13";
-                String database = "ANCLA";
-                String uid = "remoto";
-                String password = "remoto1";
-                String port = "8457";
-                String connectionString = "SERVER=" + server + "; PORT =" + port + ";" + "DATABASE="
-                    + database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
-                conexion.Open();
+                string strSQL2 = "select MAX(ID) from VENTAS";
+                MySqlCommand comando2 = new MySqlCommand(strSQL2, conexion);
+                MySqlDataReader dr = comando2.ExecuteReader();
+                int indice = dr.GetInt32("MAX(ID)");
 
                 for (int i = 0; i < detalleVenta.Count; i++)
                 {
-                    string strSQL = "insert into DETALLE_VENTAS values (@ID_PROD, @ID_VENT, @CANTIDAD, @PRECIO_VENTA)";
-                    MySqlCommand comando = new MySqlCommand(strSQL, conexion);
-                    comando.Parameters.AddWithValue("ID_PROD", detalleVenta[i].ID_PROD);
-                    comando.Parameters.AddWithValue("ID_VENT", obtenerUltimaVenta());
-                    comando.Parameters.AddWithValue("CANTIDAD", detalleVenta[i].CANTIDAD);
-                    comando.Parameters.AddWithValue("PRECIO_VENTA", detalleVenta[i].PRECIO_VENTA);
-                    comando.ExecuteNonQuery();
-                    comando.Dispose();
+                    string strSQL1 = "insert into DETALLE_VENTAS values (@ID_PROD, @ID_VENT, @CANTIDAD, @PRECIO_VENTA)";
+                    MySqlCommand comando1 = new MySqlCommand(strSQL1, conexion);
+                    comando1.Parameters.AddWithValue("ID_PROD", detalleVenta[i].ID_PROD);
+                    comando1.Parameters.AddWithValue("ID_VENT", indice);
+                    comando1.Parameters.AddWithValue("CANTIDAD", detalleVenta[i].CANTIDAD);
+                    comando1.Parameters.AddWithValue("PRECIO_VENTA", detalleVenta[i].PRECIO_VENTA);
+                    comando1.ExecuteNonQuery();
+                    comando1.Dispose();
                 }
+
+                comando.Dispose();
                 trans.Commit();
                 conexion.Close();
                 return true;
@@ -80,33 +59,7 @@ namespace Datos
                 return false;
             }
         }
-        public int obtenerUltimaVenta()
-        {
-            MySqlConnection conexion = new MySqlConnection();
-            try
-            {
-                String server = "25.89.125.13";
-                String database = "ANCLA";
-                String uid = "remoto";
-                String password = "remoto1";
-                String port = "8457";
-                String connectionString = "SERVER=" + server + "; PORT =" + port + ";" + "DATABASE="
-                    + database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
-                conexion.Open();
-                string strSQL = "select MAX(ID) from VENTAS";
-                MySqlCommand comando = new MySqlCommand(strSQL, conexion);
-                MySqlDataReader dr = comando.ExecuteReader();
-                int indice = dr.GetInt32("MAX(ID)");
-                comando.Dispose();
-                conexion.Close();
-                return indice;
-            }
-            catch (Exception e)
-            {
-                conexion.Close();
-                return -1;
-            }
-        }
+
     }
 }
 
--- AnclaSystem/Vista/GestionDeVentas/frmVenta.cs
+++ AnclaSystem/Vista/GestionDeVentas/frmVenta.cs
@@ -18,7 +18,9 @@ namespace Vista
     public partial class frmVenta : Form
     {
         List<Productos> listaProductos;
+        List<DetalleVentas> listaDetalles;
         daoProducto prod;
+        daoVentas vent;
 
         int IDseleccionado; //id del producto doble al dar clic
         int cantidad; //cantidad de productos
@@ -26,6 +28,7 @@ namespace Vista
         {
             InitializeComponent();
             listaProductos = new List<Productos>();
+            listaDetalles = new List<DetalleVentas>();
         }
 
         private void frmVenta_Load(object sender, EventArgs e)
@@ -47,12 +50,30 @@ namespace Vista
             {
                 MessageBox.Show("Entrada invalida");
             }
-
+            if (cantidad > 0)
+            {
+                DetalleVentas detalle = new DetalleVentas();
+                detalle.ID_PROD = listaProductos[IDseleccionado].ID;
+                detalle.PRECIO_VENTA= listaProductos[IDseleccionado].precio;
+                detalle.CANTIDAD = cantidad;
+                detalle.SUBTOTAL = cantidad * listaProductos[IDseleccionado].precio;
+                listaDetalles.Add(detalle);
+                dgvDetalle.DataSource = null;
+                dgvDetalle.DataSource = listaDetalles;
+            }
         }
 
         private void btnRegistrar_Click(object sender, EventArgs e)
         {
-            
+            Ventas nuevaVenta = new Ventas();
+            double suma = 0;
+            foreach(DetalleVentas x in listaDetalles)
+            {
+                suma += x.SUBTOTAL;
+            }
+            nuevaVenta.TOTAL = suma;
+            nuevaVenta.FECHA = DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss");
+            vent.agregarVenta(nuevaVenta, listaDetalles);
         }
     }
 }
