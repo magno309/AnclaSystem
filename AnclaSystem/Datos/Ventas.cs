b4cae96f62cba008ffcfc71bb302f7e24bf11d17
Jesus Ramirez Ayala <48115771+jesusraayala1298@users.noreply.github.com>
2020-11-10 12:41:39 -0600

    Acomodo de clases

---
+++ AnclaSystem/Datos/Ventas.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    public class Ventas
+    {
+        public int ID { get; set; }
+        public double TOTAL { get; set; }
+        public String FECHA{ get; set; }
+    }
+}
